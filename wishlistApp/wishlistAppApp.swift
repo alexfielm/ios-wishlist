53682bc3ed36dbfbfa8319fc55f651efb35ddd91
alexfielm <afielmachado@icloud.com>
2025-06-02 13:08:45 -0300

    Initial Commit

---
+++ wishlistApp/wishlistAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  wishlistAppApp.swift
+//  wishlistApp
+//
+//  Created by Alex Sandro Fiel Machado on 02/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct wishlistAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
