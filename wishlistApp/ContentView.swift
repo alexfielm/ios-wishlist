232550e5ff08d996ef3e725f705fc8d8bca964a4
alexfielm <afielmachado@icloud.com>
2025-06-02 13:23:39 -0300

    Firts commit, just for test this

--- wishlistApp/ContentView.swift
+++ wishlistApp/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "star.fill")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Just testing comit to github")
         }
         .padding()
     }
